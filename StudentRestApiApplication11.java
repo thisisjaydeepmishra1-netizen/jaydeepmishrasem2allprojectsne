4135d6eb5bfb9d84716bef69d7d76a2a7d303502
thisisjaydeepmishra1-netizen <thisisjaydeepmishra1@gmail.com>
2026-06-12 13:51:07 +0530

    Experiment-11

---
+++ StudentRestApiApplication11.java
@@ -0,0 +1,16 @@
+package com.example.studentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRestApiApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(
+                StudentRestApiApplication.class,
+                args
+        );
+    }
+}
